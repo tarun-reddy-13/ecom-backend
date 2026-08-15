16d8388b7edf6d9253a93c9d5a50c220c30bb28d
Tarun <tarunreddythota13@gmail.com>
2026-08-15 22:54:03 +0530

    Developed backend apis for cart checkout and view orders

---
+++ src/main/java/org/tarun/ecombackend/model/dto/OrderItemResponse.java
@@ -0,0 +1,9 @@
+package org.tarun.ecombackend.model.dto;
+
+import java.math.BigDecimal;
+
+public record OrderItemResponse(
+   String productName,
+   int quantity,
+   BigDecimal totalPrice
+) {}
